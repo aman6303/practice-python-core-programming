88c2d8bb5137276c1f060535d2feb9a08580f89c
Aman Singh <singhaman1232002@gmail.com>
2025-12-18 20:20:33 +0530

    Basics of Python Programming

---
+++ 01_basics/greet.py
@@ -0,0 +1,5 @@
+from hello_python import greet
+
+greet("My name is Aman singh")
+
+# this is a comment
---
+++ 01_basics/hello_python.py
@@ -0,0 +1,10 @@
+print("Hello, Python!")
+
+def greet(n):
+    print(n)
+    
+greet("My name is Aman")
+
+greet_one = "Hello Python"
+greet_two = "Hello World"
+greet_three = "Hello Everyone"
